92a6215cda7f8e8876545b2300be5a151aba2356
MilkyStone <396266507@qq.com>
2023-06-06 21:13:01 +0800

    set redirect

--- server.mjs
+++ server.mjs
@@ -11,21 +11,26 @@ import util from 'util'
 import fs   from 'fs'
 
 let server = http.createServer((req,res) => {
-  var pathname = url.parse(req.url).pathname
-  console.log('file:'+pathname.substring(1))
-  fs.readFile(pathname.substring(1),(err,data) =>{
-    if(err){
-      res.writeHead(404,{
-        'Content-Type':'text/html'
-      })
-    }else{
-      res.writeHead(200,{
-        'Content-Type':'text/html'
-      })
-      res.write(data.toString())
-    }
-    res.end(util.inspect(url.parse(req.url)))
-  })
+    res.writeHead(301, { Location: "http://chat.openai.com" });
+    res.end();
 })
+
+//let server = http.createServer((req,res) => {
+//  var pathname = url.parse(req.url).pathname
+//  console.log('file:'+pathname.substring(1))
+//  fs.readFile(pathname.substring(1),(err,data) =>{
+//    if(err){
+//      res.writeHead(404,{
+//        'Content-Type':'text/html'
+//      })
+//    }else{
+//      res.writeHead(200,{
+//        'Content-Type':'text/html'
+//      })
+//      res.write(data.toString())
+//    }
+//    res.end(util.inspect(url.parse(req.url)))
+//  })
+//})
 
 server.listen(process.env.PORT);
